9eb13a8ca261cf86fd0f16039b9fbc480a368b70
Bryson Flowers <101665972+Brysonflowers@users.noreply.github.com>
2025-09-22 08:42:35 -0500

    Update main.js

--- 4-Boolean/jasmine/src/main.js
+++ 4-Boolean/jasmine/src/main.js
@@ -1,1 +1,31 @@
+/*
 
+
+
+*/
+
+
+
+
+
+// Given state variables
+let knightIsAwake = true;
+let archerIsAwake = false;
+let prisonerIsAwake = true;
+let petDogIsPresent = true;
+
+// Task 1: Fast Attack Availability
+let canExecuteFastAttack = !knightIsAwake;
+console.log("Can Annalyn make a fast attack?", canExecuteFastAttack);
+
+// Task 2: Spy Availability
+let canSpy = knightIsAwake || archerIsAwake || prisonerIsAwake;
+console.log("Can Annalyn spy on the camp?", canSpy);
+
+// Task 3: Signal Prisoner Availability
+let canSignalPrisoner = prisonerIsAwake && !archerIsAwake;
+console.log("Can Annalyn signal the prisoner?", canSignalPrisoner);
+
+// Task 4: Free Prisoner Availability
+let canFreePrisoner = (petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake);
+console.log("Can Annalyn free the prisoner?", canFreePrisoner);
