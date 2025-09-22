57d1f99bcd8045f3d94e75881a3b7a1db566620d
Bryson Flowers <101665972+Brysonflowers@users.noreply.github.com>
2025-09-22 08:43:38 -0500

    Update main.js

--- 6-Conditionals/jasmine/src/main.js
+++ 6-Conditionals/jasmine/src/main.js
@@ -1,4 +1,40 @@
+//Task 1
+let kind = "car"
+let needslicenseResult;
 
+if(kind === 'car' || kind === 'truck'){
+    needslicenseResult = true
+}
+else{
+    needslicenseResult = false
+}
 
+//Task 2
+let option1 = 'Toyota';
+let option2 = "Honda";
 
+let chooseVehicleResult;
+if (option1 > option2) {
+    chooseVehicleResult = option2 + "is clearly the better choice"
+}
+else{
+    chooseVehicleResult = option1 + "is clearly the better choice"
+}
 
+//task 3
+let orginalPrice = 10000
+let age = 5
+let calulateResellPriceResult;
+
+if(age < 3) {
+    calculateResellPriceResult = originalPrice * - 0.8;
+
+}
+else if(age > 10){
+    calulateResellPriceResult = orginalPrice * 0.5;
+}
+else if(age >= 3 && age < 10){
+    calulateResellPriceResult = orginalPrice * 0.7;
+}
+
+console.log();
