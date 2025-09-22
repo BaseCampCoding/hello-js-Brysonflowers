2f542fe5a98b4699904ad882e9fd85485c38bc30
Bryson Flowers <101665972+Brysonflowers@users.noreply.github.com>
2025-09-22 08:42:00 -0500

    Update main.js

--- 3-Strings/jasmine/src/main.js
+++ 3-Strings/jasmine/src/main.js
@@ -1,1 +1,22 @@
+let line = 'Silly Sam sat on the sand';
+let frontDoorLetter = line.charAt(0);
+let frontDoorWord = 'shire';
+let frontDoorPassword = frontDoorWord.charAt(0).toUpperCase() + frontDoorWord.slice(1);
 
+let lineWithWhitespace = 'The quick brown fox jumps over the lazy dogs';
+let backDoorLetter = 's';
+let backDoorWord = 'horse';
+let backDoorPassword = backDoorWord.charAt(0).toUpperCase() + backDoorWord.slice(1) + ', please';
+
+/*let line = "Stands so high"
+  let frontDoorLetter = line(0)
+  
+  let frontDoorWord = "SHIRE"
+  let frontDoorPassword = frontDoorWord[0].toUpperCase() * frontDoorWord.slice(1.5).toLowerCase()
+
+  LineWithWhitespace = "Reins and harness"
+  let backDoorLetter = lineWithWhitespace.trim()
+  let backDoorLetter = removeWhitespace[removeWhitespace.length -1]
+  let backDoorWord = "Horse";
+  let backDoorPassword = backDoorWord + ", please"
+  */
